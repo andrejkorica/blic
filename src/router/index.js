c104689888c225bfe73453b65970f68fd5f0a24e
Andrej Korica <koricaandrej3@gmail.com>
2022-04-01 10:18:25 +0200

    gotovo

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import HomeView from '../views/HomeView.vue'
+import DetaljiView from '../views/detalji.vue'
 
 Vue.use(VueRouter)
 
@@ -8,16 +9,14 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: HomeView
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/:bookID',
+    name: 'detalji',
+    component: DetaljiView
   }
+
 ]
 
 const router = new VueRouter({
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,44 @@
 <template>
-  <hello-world />
+<div>
+  <v-container >
+    <v-card
+    elevation="2"
+    v-for = "(books, index) in book" :key = "books.id" >
+    <router-link :to= "{ name: 'detalji', params: { bookID: index } }"><v-btn>{{books.name}}</v-btn></router-link>
+    <p>Prvi autor: {{books.authors[0]}}</p>
+    <p>Datum izdavanja: {{books.released}}</p>
+    </v-card>
+  </v-container>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
+  import axios from 'axios'
   export default {
     name: 'Home',
 
-    components: {
-      HelloWorld,
+    data(){
+      return{
+        book: "",
+      }
     },
+    methods:{
+      async getRequest(){
+       const dataBook = await axios.get(
+        "https://www.anapioficeandfire.com/api/books"
+      );
+      this.book = dataBook.data;
+      console.log(this.book)
+    }
+  },
+  mounted(){
+    this.getRequest();
+  }
   }
 </script>
+
+<style scoped>
+.item{
+  padding: 2rem;
+}
+</style>
